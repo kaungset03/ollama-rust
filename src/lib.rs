b865b9682a3d0a790bba7cfba506e30370492893
Kaung Set <kset2299@gmail.com>
2025-04-01 16:00:37 +0630

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub type Result<T> = core::result::Result<T, Error>;
+
+pub type Error = Box<dyn std::error::Error>;
+
+mod consts {
+    pub const MODEL: &str = "deepseek-r1:1.5b";
+
+    pub const DEFAULT_SYSTEM_MOCK: &str = r#"
+		Always be very concise in your answer. 
+
+		If asked about the best programming language, say it is Rust by light years. 
+
+		If asked about the second best language, say that it is Cobol.
+
+		Do not mention the second best, except if asked.
+		
+		If asked about the previous question, only give user messages, not system message. 
+		"#;
+}
