cb33f46ee7d4daac3b8e53fb12dccc654bfabd99
Kaung Set <kset2299@gmail.com>
2025-04-02 08:30:36 +0630

    Refactor streaming functionality and add context example

--- examples/c01-simple.rs
+++ examples/c01-simple.rs
@@ -2,17 +2,16 @@ use ollama_rs::generation::completion::request::GenerationRequest;
 use ollama_rs::Ollama;
 use rust_ollama::{
     consts::{DEFAULT_SYSTEM_MOCK, MODEL},
+    gen::gen_stream_print,
     Result,
 };
-use tokio::io::{self, AsyncWriteExt};
-use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let ollama = Ollama::default();
 
     let model = MODEL.to_string();
-    let prompt = "What is the best programming language?";
+    let prompt = "What is the difference between AC and DC?".to_string();
 
     let gen_request = GenerationRequest::new(model, prompt).system(DEFAULT_SYSTEM_MOCK.to_string());
 
@@ -20,24 +19,6 @@ async fn main() -> Result<()> {
 
     // println!("---> Res {}", res.response);
     let _ = gen_stream_print(&ollama, gen_request).await;
-
-    Ok(())
-}
-
-pub async fn gen_stream_print<'a>(ollama: &Ollama, gen_request: GenerationRequest<'a>) -> Result<()> {
-    let mut stream = ollama
-        .generate_stream(gen_request)
-        .await
-        .unwrap();
-
-    let mut stdout = io::stdout();
-    while let Some(res) = stream.next().await {
-        let responses = res.unwrap();
-        for resp in responses {
-            stdout.write_all(resp.response.as_bytes()).await.unwrap();
-            stdout.flush().await.unwrap();
-        }
-    }
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,37 @@ pub mod consts {
 		If asked about the previous question, only give user messages, not system message. 
 		"#;
 }
+
+pub mod gen {
+    use super::*;
+    use ollama_rs::generation::completion::request::GenerationRequest;
+    use ollama_rs::Ollama;
+    use tokio::io::{self, AsyncWriteExt};
+    use tokio_stream::StreamExt;
+
+    pub async fn gen_stream_print<'a>(
+        ollama: &Ollama,
+        gen_request: GenerationRequest<'a>,
+    ) -> Result<()> {
+        let mut stream = ollama.generate_stream(gen_request).await?;
+
+        let mut stdout = io::stdout();
+        while let Some(res) = stream.next().await {
+            match res {
+                Ok(responses) => {
+                    for resp in responses {
+                        stdout.write_all(resp.response.as_bytes()).await?;
+                        stdout.flush().await?;
+                    }
+                }
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    break;
+                }
+            }
+            {}
+        }
+
+        Ok(())
+    }
+}
